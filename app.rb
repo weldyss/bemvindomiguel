d304196e6081e40806f081b003fec5daabf0912e
Weldys Santos <weldyss@gmail.com>
2018-02-13 09:02:22 -0300

    pagseguro

--- app.rb
+++ app.rb
@@ -1,4 +1,8 @@
 require 'sinatra'
+require 'securerandom'
+require './helpers'
+
+I18n.enforce_available_locales = false
 
 set :public_folder, 'public'
 
@@ -6,7 +10,8 @@ get '/' do
   erb :index
 end
 
-post '/pay/:value' do
+get '/pay/:value' do
+  order = SecureRandom.uuid
   PagSeguro.configure do |config|
     config.token       = "EC2C873997EF4D0789438EF5ECB67138"
     config.email       = "weldyss@gmail.com"
@@ -14,5 +19,18 @@ post '/pay/:value' do
     config.encoding    = "UTF-8" # ou ISO-8859-1. O padrão é UTF-8.
   end
 
-  payment = PagSeguro::PaymentRequest.new(email: 'weldyss@gmail.com', token: 'EC2C873997EF4D0789438EF5ECB67138')
+  payment = PagSeguro::PaymentRequest.new
+  payment.reference = order
+  payment.items << {
+      id: params[:value].to_i,
+      description: "Compra do Kit Fraldas no valor de R$ #{params[:value]}",
+      amount: params[:value].to_f,
+      weight: '0'
+  }
+  response = payment.register
+  if response.errors.any?
+    raise response.errors.join("\n")
+  else
+    redirect response.url
+  end
 end
