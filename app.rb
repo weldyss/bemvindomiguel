5f8d2a66e7beb7f9516608eab83f5567e1659b17
Weldys Santos <weldyss@gmail.com>
2018-02-13 20:19:26 -0300

    responsiveness home page and form

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
+require 'pagseguro'
 require 'securerandom'
+require 'sinatra/activerecord'
 require './helpers'
 
 I18n.enforce_available_locales = false
@@ -10,7 +12,11 @@ get '/' do
   erb :index
 end
 
-get '/pay/:value' do
+get '/preorder' do
+  erb :preorder
+end
+
+post '/pay' do
   order = SecureRandom.uuid
   PagSeguro.configure do |config|
     config.token       = "EC2C873997EF4D0789438EF5ECB67138"
@@ -19,18 +25,32 @@ get '/pay/:value' do
     config.encoding    = "UTF-8" # ou ISO-8859-1. O padrão é UTF-8.
   end
 
-  payment = PagSeguro::PaymentRequest.new
-  payment.reference = order
-  payment.items << {
-      id: params[:value].to_i,
-      description: "Compra do Kit Fraldas no valor de R$ #{params[:value]}",
-      amount: params[:value].to_f,
-      weight: '0'
-  }
-  response = payment.register
-  if response.errors.any?
-    raise response.errors.join("\n")
+  @order = Order.new(params[:order])
+  if @order.save
+    payment = PagSeguro::PaymentRequest.new
+    payment.reference = @order.id
+    payment.sender = {
+      name: @order.name,
+      email: @order.email
+    }
+    payment.items << {
+        id: @order.id,
+        description: "Compra do Kit Fraldas no valor de R$ #{@order.amount}",
+        amount: @order.amount.to_f,
+        weight: '0'
+    }
+    response = payment.register
+    if response.errors.any?
+      raise response.errors.join("\n")
+    else
+      redirect response.url
+    end
   else
-    redirect response.url
+    erb :preorder
   end
+end
+
+
+class Order < ActiveRecord::Base
+  validates :name, :email, presence: true
 end
